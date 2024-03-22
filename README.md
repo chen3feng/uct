554eb5b9760c2e2e9ba98c364c943cbc1d31ba73
CHEN Feng <chen3feng@gmail.com>
2024-03-22 18:09:30 +0800

    Basic functions work. (#1)

--- README.md
+++ README.md
@@ -1,25 +1,268 @@
-# [WIP] Command Line Tools for Unreal Engine
+# [WIP] Command Line Tool for Unreal Engine
 
-Planed features:
+## What is it?
+
+This is a powerful command line tool to run unreal build and editor commands much easier.
+
+## Background
+
+Many cases, I prefer command line tools because they are fast and easy automation. I often write code for UE in VS Code,
+so I often need to call [UBT](https://docs.unrealengine.com/4.27/en-US/ProductionPipelines/BuildTools/UnrealBuildTool/) manually.
+But the its command line interface are very verbose.
+
+For example, to build a Program:
+
+```console
+G:\MyGame> G:\UnrealEngine-5.1\Engine\Build\BatchFiles\Build.bat Benchmark Win64 Development -Project="G:\MyGame\MyGame.uproject"
+```
+
+To run a test from command line:
+
+```console
+G:\Pb4ueRpc>G:\UnrealEngine-5.1\Engine\Binaries\Win64\UnrealEditor-Cmd.exe %CD%/Pb4ueTest.uproject -ExecCmds="Automation RunAll"
+```
+
+Its user interface has so many problems:
+
+- You must use UBT under the correct engine directory, here is `G:\UnrealEngine-5.1\Engine\Build\BatchFiles\Build.bat`, but we often have many engines.
+- The path of the `-Project` argument must be a absolute path, it's boring, we can use %CD% to simplify but it still need the project file name.
+- The options such as `Development` are so long.
+
+So, I developed this handy tool, to simplify my life, and, maybe yours.
+
+With this tool, you needn't:
+
+- Type the the full path of UBT, uct can find it automatically if your current directory is under the game project or the engine directory.
+- Pass the -Project=YourGame.uproject, uct can find it automatically if your current directory is under the game project.
+- Type `Win64`, uct assume the target platform is also the host platform.
+- Type `Development`, `dev` is enough.
+
+## Install
+
+Just use git to clone the code, and execute the `install.bat`:
+
+```console
+git clone https://github.com/chen3feng/uct
+cd uct
+install.bat
+```
+
+The path of uct is registered into your `PATH` environment, you can call it from any where in you system.
+
+## Basic Concepts
+
+See UE documents for the following concepts:
+
+- [Target](https://docs.unrealengine.com/4.27/en-US/ProductionPipelines/BuildTools/UnrealBuildTool/TargetFiles/) Each `.Target.cs` describes a target.
+- [Target Platform](https://unrealcommunity.wiki/6100e8109c9d1a89e0c31618) UE Support `Win64`, `Linux`, `Mac` and some other such as `Hololens`.
+- [Configuration](https://docs.unrealengine.com/4.27/en-US/ProductionPipelines/DevelopmentSetup/BuildConfigurations/), Such as `Debug`, `Development`, `Shipping` and `Test`.
+- [Module](https://docs.unrealengine.com/4.27/en-US/ProductionPipelines/BuildTools/UnrealBuildTool/ModuleFiles/) Each `.Build.cs` describes a module.
+
+## Basic Usage
+
+### Command Line Interface
+
+The basic interface is:
+
+`uct` \<command\> options...
+
+For example:
+
+```console
+uct build -t Benchmark
+```
+
+`build` is the command, `-t Benchmark` is an option.
+
+Just like the git command. easy?
+
+UCT support the following commands:
+
+### List targets
+
+List all targets:
+
+```console
+$ uct list-targets
+BaseTextureBuildWorker
+BenchmarkTool
+BlankProgram
+BuildPatchTool
+...
+```
+
+List all engine targets:
+
+```console
+$ uct list-targets --engine
+BaseTextureBuildWorker
+BenchmarkTool
+BlankProgram
+BuildPatchTool
+...
+```
+
+List all project targets:
+
+```console
+uct list-targets --project
+MyGameTest
+MyGameClient
+MyGameEditor
+MyGameServer
+MyGameBenchmark
+```
+
+Verbose output:
+
+```console
+$ uct list-targets --verbose
+Type      Name                            Path
+------------------------------------------------------------------------------------------------------------------------
+Program   BaseTextureBuildWorker          G:\UnrealEngine-5.1\Engine\Source\Programs\BaseTextureBuildWorker\BaseTextureBuildWorker.Target.cs
+Program   BenchmarkTool                   G:\UnrealEngine-5.1\Engine\Source\Programs\BenchmarkTool\BenchmarkTool.Target.cs
+Program   BlankProgram                    G:\UnrealEngine-5.1\Engine\Source\Programs\BlankProgram\BlankProgram.Target.cs
+```
+
+### Generate Project Files
+
+Generate project files for the engine or game project.
+
+```console
+$ uct generate-project-files
+...
+```
+
+Generate project files for the project or engine according to the current directory.
+
+### Build
+
+Build one target
+
+```console
+$ uct build -t Benchmark UnrealEditor
+...
+```
+
+Build multiple targets:
+
+```console
+$ uct build -t Benchmark UnrealEditor
+...
+```
+
+It supports wildcard:
+
+```console
+uct build -t MyProject*
+uct build -t *Editor
+uct build -t MyProject* *Editor
+```
+
+It supports specifing [build configuration](https://dev.epicgames.com/documentation/en-us/unreal-engine/build-configurations-reference-for-unreal-engine?application_version=5.3)
+and [target platform](https://unrealcommunity.wiki/6100e8109c9d1a89e0c31618):
+
+```console
+uct build -c debug -p linux
+```
+
+To simplify typing, in UCT, all configuration name are lowercase.
+Valid configurations:
+
+- win, win64: Win64
+- linux: Linux
+- mac: Mac
+
+To pass [extra arguments](https://ikrima.dev/ue4guide/build-guide/utilities/devops-build-automation/) to UBT, put them after a standalone `--`:
+
+```console
+uct build -t MyGame -- -StaticAnalyzer=VisualCpp
+```
+
+### Clean
+
+Clean one or more targets, example:
+
+```console
+$ uct clean -t Benchmark UnrealEditor
+...
+```
+
+See the above `build` command for reference.
+
+### Run
+
+Run a program:
+
+```console
+$ uct run -t Benchmark
+Run G:\MyGame\Binaries\Win64\Benchmark.exe
+LogBenchmark: Display: Running 'BM_Serialize<FFieldTest>'...
+LogBenchmark: Display: Running 'BM_Serialize<FBenchmarkTest>'...
+LogBenchmark: Display: Running 'BM_Deserialize<FFieldTest>'...
+LogBenchmark: Display: Serialized size=109
+...
+```
+
+All arguments after the first `--` is passed to the program:
+
+```console
+uct run -t Benchmark -- --help --help
+```
+
+The program got `--help -- --help` aruguments.
+
+## Test
+
+UCT use [`-ExecCmds Automation ...`](https://forums.unrealengine.com/t/run-automated-testing-from-command-line/294995)
+to execute automation tests.
+
+Options:
+
+- `--list`: list all tests
+- `--run-all`: Run all test
+- `--run`: Run specified tests, separted by space
+- `--cmds`: Any extra test commands you want to run
+
+Examples:
+
+```console
+# List all tests
+uct test --list
+
+# Run all tests
+uct test --run-all
+
+# Run all tests starts with 'System.Core'
+uct test --run System.Core
+```
+
+The `--cmds` option can be used to pass more test commands to the system.
+
+Example:
+
+```console
+uct test --cmds List RunAll "RunTests System" Quit
+```
+
+The -ExecCmd command is `Automation List; RunAll; "RunTests System"; Quit`
+
+According to the source code of UE, you can use the following test commands:
+
+```text
+Automation List
+Automation RunTests <test string>
+Automation RunAll
+Automation RunFilter <filter name>
+Automation SetFilter <filter name>
+Automation Quit
+```
+
+## Planned Features
 
 ```console
 # Run Setup, in engine only
 uct setup
-
-# Generate project files
-uct project vscode
-
-# Build targets
-uct build game editor client server
-
-# Build with configurations
-uct build -c debug -p linux
-
-# Clean
-uct clean
-
-# Run program
-uct run
 
 # Pack
 uct pack
--- uct.py
+++ uct.py
@@ -1,11 +1,391 @@
 import argparse
+import fnmatch
+import json
 import os
+import re
+import platform
+import subprocess
 import sys
+
+# https://pypi.org/project/argcomplete/
+try:
+    # pyright: reportMissingImports=false
+    import argcomplete
+except ImportError:
+    argcomplete = None
+
+VERSION = '1.0'
+
+
+PLATFORM_MAP = {
+    'win': 'Win64',
+    'win64': 'Win64',
+    'linux': 'Linux',
+    'mac': 'Mac'
+}
+
+CONFIG_MAP = {
+    'debug': 'Debug',
+    'dbg': 'Debug',
+    'dev': 'Development',
+    'ship': 'Shipping',
+    'test': 'Test',
+}
+
+
+def build_arg_parser():
+    parser = argparse.ArgumentParser(prog='UCT', description='Unreal command line tool')
+    parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
+
+    subparsers = parser.add_subparsers(dest='command', help='Available subcommands')
+    subparsers.required = True
+
+    list_targets = subparsers.add_parser('list-targets', help='List targets')
+    list_targets.add_argument('--project', action='store_true')
+    list_targets.add_argument('--engine', action='store_true')
+    list_targets.add_argument('--verbose', action='store_true')
+
+    generate_project_files = subparsers.add_parser('generate-project-files', help='Generate project files')
+
+    build = subparsers.add_parser('build', help='Build specified targets')
+
+    build.add_argument('-p', '--platform', dest='platform', type=str,
+                        choices=PLATFORM_MAP.keys(),
+                        help='target platform')
+    build.add_argument('-c', '--config', dest='config', type=str,
+                        choices=CONFIG_MAP.keys(),
+                        help='build configuration')
+    build.add_argument('-t', '--targets', type=str, nargs='+',
+                        help='targets to build')
+    build.add_argument('-m', '--modules', type=str, nargs='+',
+                        help='modules to build')
+    build.add_argument('-f', '--files', type=str, nargs='+',
+                        help='source files to compile')
+    clean = subparsers.add_parser('clean', help='Clean specified targets', parents=[build], add_help=False)
+
+    run = subparsers.add_parser(
+        'run',
+        help='Build and run a single target',
+        epilog='Any arguments after the empty "--" will be passed to the program',
+        parents=[build], add_help=False)
+
+    test = subparsers.add_parser(
+        'test',
+        help='Build and run tests',
+        epilog='Any arguments after the empty "--" will be passed to the program',
+        parents=[build], add_help=False)
+    test.add_argument('--list', dest='list', action='store_true', help='list all tests')
+    test.add_argument('--run-all', dest='run_all', action='store_true', help='Run all test')
+    test.add_argument('--run', dest='tests', type=str,  nargs='+', help='Run tests')
+    test.add_argument('--cmds', dest='test_cmds', type=str, nargs='+', help='Any extra test commands')
+
+    return parser
+
+
+def parse_command_line():
+    """Parse and validate commandparameters"""
+    parser = build_arg_parser()
+
+    if argcomplete:
+        argcomplete.autocomplete(parser)
+
+    # If '--' in arguments, use all other arguments after it as run
+    # arguments
+    args = sys.argv[1:]
+    if '--' in args:
+        pos = args.index('--')
+        extra_args = args[pos + 1:]
+        args = args[:pos]
+    else:
+        extra_args = []
+
+    return parser.parse_args(args), extra_args
+
+
+def find_file_bottom_up(pattern, from_dir=None) -> str:
+    """Find the specified file/dir from from_dir bottom up until found or failed.
+       Returns abspath if found, or empty if failed.
+    """
+    if from_dir is None:
+        from_dir = os.getcwd()
+    finding_dir = os.path.abspath(from_dir)
+    while True:
+        files = os.listdir(finding_dir)
+        for file in files:
+            if fnmatch.fnmatch(file, pattern):
+                return os.path.join(finding_dir, file)
+        parent_dir = os.path.dirname(finding_dir)
+        if parent_dir == finding_dir:
+            return ''
+        finding_dir = parent_dir
+    return ''
+
+
+def find_files_under(dir, pattern, excluded_dirs=None, relpath=False) -> list[str]:
+    result = []
+    for root, dirs, files in os.walk(dir):
+        if excluded_dirs:
+            dirs[:] = [d for d in dirs if d not in excluded_dirs]
+        for file in files:
+            if fnmatch.fnmatch(file, pattern):
+                path = os.path.join(root, file)
+                if relpath:
+                    path = os.path.relpath(path, dir)
+                result.append(path)
+    return result
+
+class UnrealCommandTool:
+    """Unreal Command Line Tool."""
+    def __init__(self, options, extra_args):
+        self.engine_root, self.project_file = self._find_project()
+        self.engine_dir = os.path.join(self.engine_root, 'Engine')
+        self.project_dir = os.path.dirname(self.project_file) if self.project_file else ''
+        self.host_platform = self._host_platform()
+        self.ubt = self._find_ubt()
+        assert os.path.exists(self.ubt), self.ubt
+        self._load_project_targets()
+
+        self.options = options
+        self.extra_args = extra_args
+
+    def _find_project(self):
+        project_file = os.environ.get('PROJECT_FILE')
+        if not project_file:
+            project_file = find_file_bottom_up('*.uproject')
+        engine_root =  os.environ.get('ENGINE_ROOT')
+        if not engine_root:
+            key_file = find_file_bottom_up('GenerateProjectFiles.bat')
+            if key_file:
+                engine_root = os.path.dirname(key_file)
+        if not engine_root:
+            engine_root = '/Users/cf/Documents/Work/UnrealEngine'
+        return engine_root, project_file
+
+    def _find_ubt(self):
+        if self.host_platform == 'Win64':
+            return os.path.normpath(os.path.join(self.engine_dir, 'Build/BatchFiles/Build.bat'))
+        return os.path.normpath(os.path.join(self.engine_dir, f'Build/BatchFiles/{self.host_platform}/Build.sh'))
+
+    def _host_platform(self):
+        system = platform.system()
+        if system == 'Windows':
+            return 'Win64'
+        if system == 'Darwin':
+            return 'Mac'
+        return system
+
+    def _parse_build_options(self, options):
+        self.platform = PLATFORM_MAP.get(options.platform, self.host_platform)
+        self.config = CONFIG_MAP.get(options.config, 'Development')
+        self.targets = self._expand_targets(options.targets)
+
+    def _expand_targets(self, targets):
+        if targets is None:
+            return []
+        has_wildcard = False
+        all_target_names = [t['Name'] for t in self.all_targets]
+        expanded_targets = []
+        for target in targets:
+            if self._is_wildcard(target):
+                has_wildcard = True
+                expanded_targets += fnmatch.filter(all_target_names, target)
+            else:
+                if target not in all_target_names:
+                    print(f"warning: target {target}' doesn't exist", file=sys.stderr)
+                expanded_targets.append(target)
+        if has_wildcard:
+            print(f'Targets: {" ".join(expanded_targets)}')
+
+        return expanded_targets
+
+    def _is_wildcard(self, text):
+        for c in text:
+            if c in '*?![]':
+                return True
+        return False
+
+    def _load_project_targets(self):
+        self.engine_targets = self._load_target_info(self.engine_dir)
+        if not self.engine_targets:
+            self.engine_targets = self._scan_targets(self.engine_dir)
+        assert self.engine_targets
+        self.project_targets = []
+        if self.project_dir:
+            self.project_targets = self._load_target_info(self.project_dir)
+            if not self.project_targets:
+                self.project_targets = self._scan_targets(self.project_dir)
+        self.all_targets = self.engine_targets + self.project_targets
+
+    def _load_target_info(self, dir):
+        path = os.path.join(dir, 'Intermediate', 'TargetInfo.json')
+        try:
+            with open(path, encoding='utf8') as f:
+                return json.load(f)['Targets']
+        except FileNotFoundError:
+            # print(f"Can't open {path} for read")
+            pass
+        return []
+
+    def _scan_targets(self, dir) -> list[dict[str, str]]:
+        targets = []
+        pattern = '*.Target.cs'
+        excluded_dirs = ['Binaries', 'DerivedDataCache', 'Intermediate']
+        files = []
+        files += find_files_under(os.path.join(dir, 'Source'), pattern, excluded_dirs=excluded_dirs)
+        files += find_files_under(os.path.join(dir, 'Plugins'), pattern, excluded_dirs=excluded_dirs)
+        for file in files:
+            target = self._parse_target_cs(file)
+            if target:
+                targets.append(target)
+        return targets
+
+    def _parse_target_cs(self, file):
+        name = ''
+        with open(file, encoding='utf8') as f:
+            for line in f:
+                line = line.strip()
+                m = re.match(r'public\s+class\s+(\w+)Target\b', line)
+                if m:
+                    name = m.group(1)
+                    continue
+                if name:
+                    m = re.match(r'Type\s*=\s*TargetType.(\w+)\s*;', line)
+                    if m:
+                        target_type = m.group(1)
+                        return {'Name': name, 'Path': file, 'Type': target_type}
+        return None
+
+    def execute(self) -> int:
+        command = self.options.command.replace('-', '_')
+        assert command in dir(self), f'{command} method is not defined'
+        return getattr(self, command)()
+
+    def list_targets(self) -> int:
+        # print('List targets')
+        if self.options.engine:
+            self.print_targets(self.engine_targets)
+        if self.options.project:
+            if not self.project_file:
+                print('You are not under a game project directory', file=sys.stderr)
+                return 1
+            self.print_targets(self.project_targets)
+        if not self.options.engine and not self.options.project:
+            self.print_targets(self.all_targets)
+        return 0
+
+    def generate_project_files(self) -> int:
+        suffix = 'bat' if self.host_platform == 'Win64' else 'sh'
+        cmd = [os.path.join(self.engine_root, 'GenerateProjectFiles.' + suffix)]
+        if self.project_file:
+            cmd.append(self.project_file)
+        return subprocess.call(cmd)
+
+    def print_targets(self, targets):
+        if self.options.verbose:
+            print(f'{"Type":10}{"Name":32}{"Path"}')
+            print('-' * 120)
+            for t in targets:
+                print(f'{t["Type"]:10}{t["Name"]:32}{t["Path"]}')
+        else:
+            for t in targets:
+                print(t['Name'])
+
+    def _is_project_target(self, name):
+        for target in self.project_targets:
+            if target['Name'] == name:
+                return True
+        return False
+
+    def build(self) -> int:
+        # print('Build')
+        self._parse_build_options(self.options)
+        returncode = 0
+        project = f'-Project={self.project_file}' if self.project_file else ''
+        for target in self.targets:
+            print(f'Build {target}')
+            cmd = [self.ubt, project, target, self.platform, self.config]
+            if self.options.files:
+                cmd += [f'--singlefile={f}' for f in self.options.files]
+            cmd += self.extra_args
+            ret = subprocess.call(cmd)
+            if ret != 0: # Use first failed exitcode
+                returncode = ret
+        return returncode
+
+    def clean(self) -> int:
+        print('Clean')
+        self._parse_build_options(self.options)
+        returncode = 0
+        project = f'-Project={self.project_file}' if self.project_file else ''
+        for target in self.targets:
+            print(f'Clean {target}')
+            cmd = [self.ubt, project, target, self.platform, self.config, '-Clean']
+            cmd += self.extra_args
+            ret = subprocess.call(cmd)
+            if ret != 0: # Use first failed exitcode
+                returncode = ret
+        return returncode
+
+    def run(self) -> int:
+        self._parse_build_options(self.options)
+        returncode = 0
+        for target in self.targets:
+            root = self.project_dir if self._is_project_target(target) else self.engine_dir
+            suffix = '.exe' if self.platform == 'Win64' else ''
+            executable = os.path.join(root, 'Binaries', self.platform, target + suffix)
+            cmd = [executable] + self.extra_args
+            print(f'Run {" ".join(cmd)}')
+            ret = subprocess.call(cmd)
+            if ret != 0: # Use first failed exitcode
+                returncode = ret
+        return returncode
+
+    def test(self) -> int:
+        self._parse_build_options(self.options)
+        test_cmds = self._make_test_cmds()
+        if not test_cmds:
+            print('No test command to execute')
+            return 0
+        # Example command line:
+        # G:\UnrealEngine-5.1\Engine\Binaries\Win64\UnrealEditor-Cmd.exe %CD%/MyGame.uproject -log -NoSplash -Unattended -ExecCmds="Automation RunTests System; Quit"
+        suffix = '.exe' if self.platform == 'Win64' else ''
+        editor = os.path.join(self.engine_dir, 'Binaries', self.platform, 'UnrealEditor-Cmd' + suffix)
+        test_cmds = f'Automation {test_cmds}; Quit'
+        print(f'Test command: {test_cmds}')
+        cmd = [editor, self.project_file, '-log', '-NoSplash', '-Unattended', f'-ExecCmds="{test_cmds}"'] + \
+               self.extra_args
+        if self.host_platform == 'Win64':
+            # Neither pass the list directly nor use subprocess.list2cmd works because they convert
+            # -ExecCmds="Automation List; Quit" to "-ExecCmds=\"Automation List; Quit\"",
+            # But simplay join the list with spaces works.
+            cmd = ' '.join(cmd)
+        print(f'Command line: {cmd}')
+        return subprocess.call(cmd)
+
+    def _make_test_cmds(self) -> str:
+        cmds = []
+        if self.options.list:
+            cmds.append('List')
+        if self.options.run_all:
+            cmds.append('RunAll')
+        if self.options.tests:
+            # The RunTests command use '+' to split multiple tests
+            cmds.append(f'RunTests {"+".join(self.options.tests)}')
+        if self.options.test_cmds:
+            cmds += self.options.test_cmds
+        return '; '.join(cmds)
 
 
 def main():
-    print('Welcome to UCT: the Unreal CommandLine Tools')
-    print(sys.argv)
+    # print('Welcome to UCT: the Unreal CommandLine Tool')
+    options, extra_args = parse_command_line()
+    # print(options)
+    uct = UnrealCommandTool(options, extra_args)
+    ret = uct.execute()
+    if ret != 0:
+        print(f'{options.command} failed with exit code {ret}.', file=sys.stderr)
+        sys.exit(ret)
+
 
 if __name__ == '__main__':
     main()
