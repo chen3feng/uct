4a2bd7410c85357a216485fe4cc2fc64beeffcbd
chen3feng <chen3feng@gmail.com>
2024-03-23 19:28:42 +0800

    Fix install.bat

--- install.bat
+++ install.bat
@@ -1,11 +1,18 @@
 @echo off
 
 set THIS_DIR=%~dp0
+:: Remove trailing back slash if any
+if "%THIS_DIR:~-1%" == "\" set "THIS_DIR=%THIS_DIR:~0,-1%"
+
 set "PATH=%THIS_DIR%;%PATH%"
 
-reg query "HKCU\Environment" /v "Path"
-reg query "HKCU\Environment" /v "Path" | findstr %THIS_DIR% >NUL
-if errorlevel 1 (
-    echo ::reg add "HKCU\Environment" /v "Path" /t REG_EXPAND_SZ /d "%PATH%;%THIS_DIR%" /f
-)
+set UserPath=
+for /f "tokens=3*" %%i in ('reg query "HKCU\Environment" /v "Path" 2^>nul') do set "UserPath=%%i"
 
+echo %UserPath% | findstr /C:%THIS_DIR%
+if errorlevel 1 (
+    setx PATH "%UserPath%;%THIS_DIR%"
+    echo UCT is added to you user PATH.
+) else (
+    echo UCT is already in your path
+)
