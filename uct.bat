6e7881cc877fc46a9be5aa312a473a5a3954d080
chen3feng <chen3feng@gmail.com>
2024-03-28 01:23:14 +0800

    Improve batch starter

--- uct.bat
+++ uct.bat
@@ -2,20 +2,20 @@
 
 setlocal EnableDelayedExpansion
 
+set ENGINE_ROOT_KEY_FILE=
 set ENGINE_ROOT=
 set PROJECT_FILE=
 
-call :FindFileBottomUp GenerateProjectFiles.*
-if defined FOUND_FILE (
-    for %%I in ("%FOUND_FILE%") do set "ENGINE_ROOT=%%~dpI"
-    :: Remove trailing backslashes
+call :FindFileBottomUp GenerateProjectFiles.* ENGINE_ROOT_KEY_FILE
+if defined ENGINE_ROOT_KEY_FILE (
+    for %%I in ("%ENGINE_ROOT_KEY_FILE%") do set "ENGINE_ROOT=%%~dpI"
+    :: Remove trailing backslash
     if "!ENGINE_ROOT:~-1!"=="\" set "ENGINE_ROOT=!ENGINE_ROOT:~0,-1!"
 ) else (
-    call :FindFileBottomUp *.uproject
-    if defined FOUND_FILE (
-        set "PROJECT_FILE=!FOUND_FILE!"
+    call :FindFileBottomUp *.uproject PROJECT_FILE
+    if defined PROJECT_FILE (
         ::echo Found unreal project "!PROJECT_FILE!"
-        call :FindProjectEngineDir !PROJECT_FILE!
+        call :FindEngineByProject !PROJECT_FILE! ENGINE_ROOT
 		if not defined ENGINE_ROOT (
 		    echo Can't find associated unreal engine, check the "EngineAssociation" field in !PROJECT_FILE!. 1>&2
 		)
@@ -50,59 +50,61 @@ goto :EOF
 :: Functions
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-:: function FindFileBottomUp(pattern: wildcard)
+:: function FindFileBottomUp(pattern: wildcard, &found_file: varname)
 :: Find file bottom up
 :: return: set the FOUND_FILE variable
 :FindFileBottomUp
-set FOUND_FILE=
-set "currentDir=%CD%"
+    setlocal
+    set found_file=
+    set "currentDir=%CD%"
+
 :FindUpper
-call :FindFileInDir %1 %currentDir%
-if defined FOUND_FILE exit /b
-set parentDir=
-for %%I in ("%currentDir%") do set "parentDir=%%~dpI"
-if not defined parentDir exit /b
-:: Remove trailing backslashes
-if "%parentDir:~-1%"=="\" set "parentDir=%parentDir:~0,-1%"
+    call :FindFileInDir %1 %currentDir% found_file
+    if defined found_file exit /b
+    set parentDir=
+    for %%I in ("%currentDir%") do set "parentDir=%%~dpI"
+    if not defined parentDir exit /b
+    :: Remove trailing backslashes
+    if "%parentDir:~-1%"=="\" set "parentDir=%parentDir:~0,-1%"
 
+    :: Return if parentDir is the drive name
+    :: The spaces before "|" are also echoed, such as echo A | findstr... will echo "A "
+    :: make it can't match the "$" in regex. so I have to use "{}" around the text. stupid cmd prompt.
+    echo "{%parentDir%}" | findstr /R "{[A-Za-z]:}" >nul
+    if %errorlevel% == 0 exit /b
 
-:: Return if parentDir is the drive name
-:: The spaces before "|" are also echoed, such as echo A | findstr... will echo "A "
-:: make it can't match the "$" in regex. so I have to use "{}" around the text. stupid cmd prompt.
-echo "{%parentDir%}" | findstr /R "{[A-Za-z]:}" >nul
-if %errorlevel% == 0 exit /b
-
-if not "%parentDir%" == "%currentDir%" (
-    set "currentDir=!parentDir!"
-    goto :FindUpper
-)
+    if not "%parentDir%" == "%currentDir%" (
+        set "currentDir=!parentDir!"
+        goto :FindUpper
+    )
+    endlocal & set "%2=%found_file%"
 exit /b
 
 
-:: function FindFileInDir(pattern: wildcard, target_directory: directory)
+:: function FindFileInDir(pattern: wildcard, target_directory: directory, &found_file: varname)
 :: Find file in dir
 :FindFileInDir
-for %%F in ("%2\%1") do (
-    set "FOUND_FILE=%%F"
-    exit /b
-)
+    for %%F in ("%2\%1") do (
+        set "%3=%%F"
+        exit /b
+    )
 exit /b
 
 
-:: function FindProjectEngineDir()
-:: return: set the ENGINE_ROOT variable.
-:FindProjectEngineDir
-
-set "key=HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine\Builds"
-:: Iterate over each line in the file
-for /f "tokens=*" %%a in ('type "%1" ^| findstr /C:"EngineAssociation"') do (
-    for /f "tokens=2 delims={}" %%b in ("%%a") do (
-        :: Read the registry value
-        for /f "tokens=3*" %%c in ('reg query "%key%" /v "{%%b}" 2^>nul ^| findstr /C:"REG_SZ"') do (
-            set "ENGINE_ROOT=%%c"
-            :: Replace forward slashes with backslashes
-            set "ENGINE_ROOT=!ENGINE_ROOT:/=\!"
+:: function FindEngineByProject(project_file: path, &engine_root: varname)
+:FindEngineByProject
+    setlocal EnableDelayedExpansion
+    set "key=HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine\Builds"
+    :: Iterate over each line in the file
+    for /f "tokens=*" %%a in ('type "%1" ^| findstr /C:"EngineAssociation"') do (
+        for /f "tokens=2 delims={}" %%b in ("%%a") do (
+            :: Read the registry value
+            for /f "tokens=3*" %%c in ('reg query "%key%" /v "{%%b}" 2^>nul ^| findstr /C:"REG_SZ"') do (
+                set "engine_root=%%c"
+                :: Replace forward slashes with backslashes
+                set "engine_root=!engine_root:/=\!"
+            )
         )
     )
-)
+    endlocal & set %1=%engine_root%
 exit /b
